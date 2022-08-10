a580e87ee25ba3ce7e1d0289ad37e3b7339261c3
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2022-08-10 20:34:12 +0530

    Add files via upload

---
+++ Reverse_number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    int rev = 0;
+    while (n>0)
+    {
+        int last_digit = n % 10;
+        rev = rev * 10 + last_digit;
+        n = n / 10;
+    }
+
+    cout << "The reverse of number is : " << rev << endl;
+
+    return 0;
+}
