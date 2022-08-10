9fe01ed1e27c34076ccff54341656fd02eb485d7
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2022-08-11 00:30:29 +0530

    Add files via upload

---
+++ calculate_ncr.cpp
@@ -0,0 +1,29 @@
+/*
+    nCr =    n!
+        ------------
+        (n-r)! * r!
+*/
+
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    int factorial = 1;
+
+    for(int i=2; i<=n; i++)
+    {
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+
+int main()
+{
+    int n,r;
+    cin>>n>>r;
+
+    int ncr = fact(n) / (fact(n-r)*fact(r));
+    cout<<ncr<<endl;
+    return 0;
+}
